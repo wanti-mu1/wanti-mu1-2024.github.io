c9f199767b7b99d7df2dd184e8171b1fff82e871
wanti-mu1 <94421126+wanti-mu1@users.noreply.github.com>
2024-07-31 21:46:37 +0900

    最終課題のファイルをアップロード

---
+++ js/main16.js
@@ -0,0 +1,22 @@
+
+
+
+console.log(document.getElementsByTagName('button'));
+
+document.getElementsByTagName('button')[0].addEventListener('click',()=> {
+    window.alert('二番！かなり注目の作品です');
+
+});
+
+document.getElementsByTagName('button')[1].addEventListener('click',()=> {
+    window.alert('三番！そこそこ注目の作品です');
+
+});
+
+
+document.getElementsByTagName('button')[2].addEventListener('click',()=> {
+    window.alert('一番！最も注目の作品です');
+
+});
+
+
